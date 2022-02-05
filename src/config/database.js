46923d1b7f411d0132dcc1974818f46a42e2dfa6
ervin nurhediyanto <cafeinai311@gmail.com>
2022-02-06 02:33:15 +0700

    init

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 import { Sequelize } from "sequelize";
 
-const username = process.env.DB_USERNAME || 'root';
-const password = process.env.DB_PASSWORD || '';
-const host = process.env.DB_HOST || 'localhost';
+const username = process.env.DB_USERNAME || 'cafeinai';
+const password = process.env.DB_PASSWORD || 'passCafe312';
+const host = process.env.DB_HOST || 'db4free.net';
+const database = process.env.DB_HOST || 'tickit312';
 
-const db = new Sequelize('ticket_db', username, password, {
+const db = new Sequelize(database, username, password, {
   host: host,
   dialect: "mysql"
 });
